83de2303b4c0520b5fabe97d608a0867f0965ed7
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-08 22:50:31 +0530

    ADDED : PERFECT NUMBER PROGRAM

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -4,7 +4,14 @@ public class LogicalProgram {
     Scanner scan = new Scanner(System.in);
     String program;
 
-    public void fibonacci_seris() {
+    public int userInterface(String choice) {
+        int number;
+        System.out.println("Enter " + choice + " number");
+        number = scan.nextInt();
+        return number;
+    }
+
+    public void fibonacciSeris() {
         program = "Fibonacci ";
         int firstValue = 0;
         int secondValue = 1;
@@ -19,10 +26,22 @@ public class LogicalProgram {
 
     }
 
-    public int userInterface(String choice) {
-        int number;
-        System.out.println("Enter " + choice + " number");
-        number = scan.nextInt();
-        return number;
+    public void perfectProgram() {
+        program = "Perfect Number";
+        int number = userInterface(program);
+        int i =1,result=0;
+        System.out.print(number+" = ");
+        while (i<=number/2){
+            if (number%i==0){
+                result +=i;
+                System.out.print(i+" + ");
+            }
+            i++;
+        }
+        if (number==result){
+            System.out.println("\n"+number+" is Perfect Number");
+            return;
+        }
+        System.out.println("\n"+number+" is Not Perfect Number");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static final int FIBONACCI_SERIES = 1;
+    public static final int PERFECT_NUMBER = 2;
+    public static final int EXIT = 3;
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO LOGICAL PROGRAM");
         Scanner scan = new Scanner(System.in);
         LogicalProgram logicalProgram = new LogicalProgram();
-        System.out.println("1] FIBONACCI SERIES PROGRAM");
-        int choice = scan.nextInt();
-        switch (choice) {
-            case FIBONACCI_SERIES:
-                logicalProgram.fibonacci_seris();
-                break;
-            default:
-                System.out.println("Thank You !");
+        while (true) {
+            System.out.println("\n1] FIBONACCI SERIES PROGRAM");
+            System.out.println("2] PERFECT NUMBER PROGRAM");
+            System.out.println("3] EXIT PROGRAM");
+            switch (scan.nextInt()) {
+                case FIBONACCI_SERIES:
+                    logicalProgram.fibonacciSeris();
+                    break;
+                case PERFECT_NUMBER:
+                    logicalProgram.perfectProgram();
+                    break;
+                case EXIT:
+                    System.exit(0);
+                default:
+                    System.out.println("Thank You !");
+            }
         }
     }
 }
