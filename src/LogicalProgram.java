b893418ce1de14d138f92ef69a54a21a7b40124c
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-08 22:13:06 +0530

    ADDED : FIBONACCI SERIES PROGRAM

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class LogicalProgram {
+    Scanner scan = new Scanner(System.in);
+    String program;
+
+    public void fibonacci_seris() {
+        program = "Fibonacci ";
+        int firstValue = 0;
+        int secondValue = 1;
+        int number = userInterface(program);
+        System.out.print("FIBONACCI SERIES :- ");
+        for (int i = 0; i < number; i++) {
+            System.out.print(firstValue+", ");
+            int temp = firstValue+secondValue;
+            firstValue = secondValue;
+            secondValue =temp;
+        }
+
+    }
+
+    public int userInterface(String choice) {
+        int number;
+        System.out.println("Enter " + choice + " number");
+        number = scan.nextInt();
+        return number;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final int FIBONACCI_SERIES = 1;
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO LOGICAL PROGRAM");
+        Scanner scan = new Scanner(System.in);
+        LogicalProgram logicalProgram = new LogicalProgram();
+        System.out.println("1] FIBONACCI SERIES PROGRAM");
+        int choice = scan.nextInt();
+        switch (choice) {
+            case FIBONACCI_SERIES:
+                logicalProgram.fibonacci_seris();
+                break;
+            default:
+                System.out.println("Thank You !");
+        }
     }
 }
